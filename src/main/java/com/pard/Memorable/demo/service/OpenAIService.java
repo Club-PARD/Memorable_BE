c64dcf18b2db1d96ce0d214a1ac56aa8c54cd8e4
xxjiinn <22100448@handong.ac.kr>
2024-06-29 23:57:21 +0900

    prompt

--- src/main/java/com/pard/Memorable/demo/service/OpenAIService.java
+++ src/main/java/com/pard/Memorable/demo/service/OpenAIService.java
@@ -2,10 +2,14 @@ package com.pard.Memorable.demo.service;
 
 import com.pard.Memorable.demo.dto.ChatGPTRequest;
 import com.pard.Memorable.demo.dto.ChatGPTResponse;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @Service
 public class OpenAIService {
@@ -19,7 +23,15 @@ public class OpenAIService {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Setter
+    @Getter
+    public List<String> keywords;
+
+
     public int getTokenCount(String text) throws Exception {
+
+
+
         // Prepare the request object
         ChatGPTRequest request = new ChatGPTRequest(model, text);
 
